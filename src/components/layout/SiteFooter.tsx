bffcebff4f91630aa447a090406e9ed20c71cd98
Muhammad Ammar Ibne Saif <mammar6058@gmail.com>
2026-08-05 18:05:34 +0500

    fixed-footer-logo

--- src/components/layout/SiteFooter.tsx
+++ src/components/layout/SiteFooter.tsx
@@ -21,7 +21,7 @@ export const SiteFooter = () => {
           {/* Col 1 , Brand */}
           <div className="lg:col-span-4 space-y-5">
             <Link to="/" className="flex items-center gap-3">
-              <img src={Logo} alt="SR Innovations" className="h-24 w-48 md:h-32 md:w-72" />
+              <img src={Logo} alt="SR Innovations" className=" md:h-32 md:w-72" />
               
             </Link>
             <p className="text-sm text-white/70 max-w-sm">
